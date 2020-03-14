31e63c97166fd4c9af7475fe705139cc9d2e1d5f
daniel08311 <b03705013@ntu.edu.tw>
2020-03-14 16:35:31 +0800

    add: setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="mypkg",
+    version="0.0.1",
+    author="Daniel Xu",
+    author_email="danielgg.coding@email",
+    url="http://www.test.org",
+    packages=['mypkg', 'mypkg.transform'],
+)
